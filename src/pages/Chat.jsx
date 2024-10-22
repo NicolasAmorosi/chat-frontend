454f4d7aefed37289bb770c0dc224f6f8398b360
Valentino Badaracco <valentinobadaracco04@gmail.com>
2024-10-22 14:07:02 -0300

    puse aaaa

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -7,8 +7,9 @@ export function Chat() {
     <div className=" flex flex-col items-between grow">
       <div className="flex flex-col items-center justify-center w-full h-full bg-zinc-800">
         <h1 className="text-4xl font-bold text-white">Chat {chatId}</h1>
-        <div>Hola</div>
+        <div>aaaaa</div>
         <div>valentino</div>
+        <div>chau</div>
       </div>
     </div>
   );
