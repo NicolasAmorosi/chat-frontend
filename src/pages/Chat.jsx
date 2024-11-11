763bbde66de7291d3c2cdd07d7fbc23d60d01655
Valentino Badaracco <valentinobadaracco04@gmail.com>
2024-11-11 18:20:11 -0300

    termine el header

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,15 +1,20 @@
 import { useParams } from "react-router-dom";
+import { Header } from "../components/layout/Header";
+
+
 
 export function Chat() {
   const { chatId } = useParams();
 
   return (
     <div className=" flex flex-col items-between grow">
+      <Header
+        contacto={chatId}
+        />
+
       <div className="flex flex-col items-center justify-center w-full h-full bg-zinc-800">
         <h1 className="text-4xl font-bold text-white">Chat {chatId}</h1>
-        <div>aaaaa</div>
-        <div>valentino</div>
-        <div>chau</div>
+      
       </div>
     </div>
   );
